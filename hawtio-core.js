ccdf668871f24e5f810b07b0a43e1598474406e0
Stan Lewis <slewis@fusesource.com>
2015-01-13 11:59:40 -0500

    Add more placeholder services that can be extended by other modules, adds compatability for 1.x plugins

--- hawtio-core.js
+++ hawtio-core.js
@@ -507,6 +507,63 @@ var HawtioCore;
       return {};
     });
 
+    // Placeholder service for the help registry
+    _module.factory('helpRegistry', function() {
+      return {
+        addUserDoc: function() {},
+        addDevDoc: function() {},
+        addSubTopic: function() {},
+        getOrCreateTopic: function() { return undefined; },
+        mapTopicName: function() { return undefined; },
+        mapSubTopicName: function() { return undefined; },
+        getTopics: function() { return undefined; },
+        disableAutodiscover: function() {},
+        discoverHelpFiles: function() {}
+      };
+    });
+
+    // Placeholder service for the preferences registry
+    _module.factory('preferencesRegistry', function() {
+      return {
+        addTab: function() {},
+        getTab: function() { return undefined; },
+        getTabs: function() { return undefined; }
+      };
+    });
+
+    // Placeholder service for the page title service
+    _module.factory('pageTitle', function() {
+      return {
+        addTitleElement: function() {},
+        getTitle: function() { return undefined; },
+        getTitleWithSeparator: function() { return undefined; },
+        getTitleExcluding: function() { return undefined; },
+        getTitleArrayExcluding: function() { return undefined; }
+      };
+    });
+
+    // service for the javascript object that does notifications
+    _module.factory('toastr', ["$window", function ($window) {
+      var answer = $window.toastr;
+      if (!answer) {
+        // lets avoid any NPEs
+        answer = {};
+        $window.toastr = answer;
+      }
+      return answer;
+    }]);
+
+    // Placeholder service for branding
+    _module.factory('branding', function() {
+      return {};
+    });
+
+    // Placeholder user details service
+    _module.factory('userDetails', function() {
+      return {};
+    });
+
+
     hawtioPluginLoader.addModule("ng");
     hawtioPluginLoader.addModule("ngSanitize");
     hawtioPluginLoader.addModule(HawtioCore.pluginName);

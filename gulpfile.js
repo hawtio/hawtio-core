ce2e89e994ee363db27f9167b05063bfb9ca46bd
Stan Lewis <gashcrumb@gmail.com>
2016-02-08 11:30:20 -0500

    Move hawtio-core.js to dist/, prep up a bit for typescript conversion

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,83 @@
 var gulp = require('gulp'),
     wiredep = require('wiredep').stream,
     karma = require('karma').server,
+    argv = require('yargs').argv,
+    urljoin = require('urljoin'),
     gulpLoadPlugins = require('gulp-load-plugins');
 
-var plugins = gulpLoadPlugins({ lazy: false });
+var plugins = gulpLoadPlugins({});
+var pkg = require('./package.json');
+var bower = require('./bower.json');
 
 var config = {
-  jsFile: 'app.js',
-  defsFile: 'hawtio.d.ts',
-  jsDir: 'js',
-  definitionsDir: 'definitions',
-  app: '.',
-  dist: 'dist',
-  src: ['*.html', '**/*.js' ]
+  ts: ['plugins/**/*.ts'],
+  testTs: ['test-plugins/**/*.ts'],
+  less: './plugins/**/*.less',
+  templates: ['plugins/**/*.html'],
+  testTemplates: ['test-plugins/**/*.html'],
+  templateModule: pkg.name + '-templates',
+  testTemplateModule: pkg.name + '-test-templates',
+  dist: argv.out || './dist/',
+  js: pkg.name + '.js',
+  testJs: pkg.name + '-test.js',
+  css: pkg.name + '.css',
+  tsProject: plugins.typescript.createProject({
+    target: 'ES5',
+    module: 'commonjs',
+    declarationFiles: true,
+    noExternalResolve: false,
+    removeComments: true
+  }),
+  testTsProject: plugins.typescript.createProject({
+    target: 'ES5',
+    module: 'commonjs',
+    declarationFiles: false,
+    noExternalResolve: false
+  }),
 };
 
-var tsProject = plugins.typescript.createProject({
-  target: 'ES5',
-  module: 'commonjs',
-  sortOutput: true,
-  declarationFiles: true,
-  noExternalResolve: true
-});
-
 gulp.task('bower', function() {
-  gulp.src('index.html')
-    .pipe(wiredep({
-      exclude: 'libs/polymer/polymer.js'
-    }))
+  return gulp.src('index.html')
+    .pipe(wiredep({}))
     .pipe(gulp.dest('.'));
+    /*
   gulp.src('karma.conf.js')
     .pipe(wiredep({
       exclude: 'libs/webcomponentsjs/webcomponents.js'
     }))
     .pipe(gulp.dest('.'));
+   */
 });
 
-gulp.task('connect', function() {
-  plugins.watch(config.src, function() {
+gulp.task('watch', function() {
+  //plugins.watch(['libs/**/*.d.ts', config.ts, config.templates], function() {
+  //  gulp.start(['tsc', 'template', 'concat', 'clean']);
+  //});
+  //plugins.watch([config.testTs, config.testTemplates], function() {
+  //  gulp.start([ 'example-template', 'example-concat', 'example-clean']);
+  //});
+  plugins.watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', urljoin(config.dist, '*')], function() {
     gulp.start('reload');
   });
+});
+
+
+gulp.task('connect', ['watch'], function() {
   plugins.connect.server({
-    root: config.app,
+    root: '.',
     livereload: true,
     port: 2772,
     fallback: 'index.html'
   });
 });
 
+/*
 gulp.task('test', function() {
   karma.start({
     configFile: __dirname + '/karma.conf.js'
   });
 });
+*/
 
 gulp.task('reload', function() {
   gulp.src('.')

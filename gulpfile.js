2782ef8dd2956130623658b1a44743620e61b6fc
Alexandre Kieling <abrianik@redhat.com>
2017-05-29 17:27:41 -0300

    configure Yarn to publish package to npm registry

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,15 @@
-var gulp = require('gulp'),
-    wiredep = require('wiredep').stream,
-    karma = require('karma').server,
-    argv = require('yargs').argv,
-    urljoin = require('urljoin'),
-    gulpLoadPlugins = require('gulp-load-plugins');
-
-var plugins = gulpLoadPlugins({});
-var pkg = require('./package.json');
-var bower = require('./bower.json');
-
-var config = {
-  ts: ['plugins/**/*.ts'],
-  testTs: ['test-plugins/**/*.ts'],
-  less: './plugins/**/*.less',
-  templates: ['plugins/**/*.html'],
-  testTemplates: ['test-plugins/**/*.html'],
-  templateModule: pkg.name + '-templates',
-  testTemplateModule: pkg.name + '-test-templates',
-  dist: argv.out || './dist/',
-  js: pkg.name + '.js',
-  testJs: pkg.name + '-test.js',
-  css: pkg.name + '.css',
-  tsProject: plugins.typescript.createProject({
-    target: 'ES5',
-    module: 'commonjs',
-    declarationFiles: true,
-    noExternalResolve: false,
-    removeComments: true
-  }),
-  testTsProject: plugins.typescript.createProject({
-    target: 'ES5',
-    module: 'commonjs',
-    declarationFiles: false,
-    noExternalResolve: false
-  }),
-};
-
-gulp.task('bower', function() {
-  return gulp.src('index.html')
-    .pipe(wiredep({}))
-    .pipe(gulp.dest('.'));
-    /*
-  gulp.src('karma.conf.js')
-    .pipe(wiredep({
-      exclude: 'libs/webcomponentsjs/webcomponents.js'
-    }))
-    .pipe(gulp.dest('.'));
-   */
-});
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
 
 gulp.task('watch', function() {
-  //plugins.watch(['libs/**/*.d.ts', config.ts, config.templates], function() {
-  //  gulp.start(['tsc', 'template', 'concat', 'clean']);
-  //});
-  //plugins.watch([config.testTs, config.testTemplates], function() {
-  //  gulp.start([ 'example-template', 'example-concat', 'example-clean']);
-  //});
-  plugins.watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', urljoin(config.dist, '*')], function() {
+  watch(['libs/**/*.js', 'libs/**/*.css', 'index.html', 'dist/hawtio-core.js'], function() {
     gulp.start('reload');
   });
 });
 
-
 gulp.task('connect', ['watch'], function() {
-  plugins.connect.server({
+  connect.server({
     root: '.',
     livereload: true,
     port: 2772,
@@ -71,17 +17,9 @@ gulp.task('connect', ['watch'], function() {
   });
 });
 
-/*
-gulp.task('test', function() {
-  karma.start({
-    configFile: __dirname + '/karma.conf.js'
-  });
-});
-*/
-
 gulp.task('reload', function() {
   gulp.src('.')
-    .pipe(plugins.connect.reload());
+    .pipe(connect.reload());
 });
 
 gulp.task('default', ['connect']);

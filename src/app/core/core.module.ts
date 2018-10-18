50a581135a8a9af918fcd50cdd861c654236ab73
Alexandre Kieling <alex.kieling@gmail.com>
2018-10-18 08:44:58 -0300

    refactor(core): move TableView filter utility from hawtio-integration to hawtio-core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 /// <reference path="humanize/humanize.service.ts"/>
+/// <reference path="patternfly/patternfly.service.ts"/>
 
 namespace Core {
 
   export const _module = angular
     .module('hawtio-core', [])
-    .service('humanizeService', HumanizeService);
-  
+    .service('humanizeService', HumanizeService)
+    .service('patternFlyService', PatternFlyService);
+
   export const coreModule = _module.name;
 
   export const log: Logging.Logger = Logger.get(coreModule);
---
+++ src/app/core/patternfly/patternfly.service.ts
@@ -0,0 +1,29 @@
+namespace Core {
+
+  export class PatternFlyService {
+
+    filter<T>(items: T[], filterConfig: any): T[] {
+      let filteredItems = items;
+      if (filterConfig.appliedFilters) {
+        filterConfig.appliedFilters.forEach(filter => {
+          const filterType = _.find(filterConfig.fields, { 'id': filter.id })['filterType'];
+          switch (filterType) {
+            case 'text':
+              const regExp = new RegExp(filter.value, 'i');
+              filteredItems = filteredItems.filter(item => regExp.test(item[filter.id]));
+              break;
+            case 'number':
+              filteredItems = filteredItems.filter(item => item[filter.id] === parseInt(filter.value));
+              break;
+            case 'select':
+              filteredItems = filteredItems.filter(item => item[filter.id] === filter.value);
+              break;
+          }
+        });
+      }
+      filterConfig.resultsCount = filteredItems.length;
+      return filteredItems;
+    }
+  }
+
+}

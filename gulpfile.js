a81e94a1c1723b3f22f046778190dc2bbc8ba62d
Alexandre Kieling <alex.kieling@gmail.com>
2019-02-08 14:32:11 -0200

    fix templateCache added at the top of dist/hawtio-core.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var argv = require('yargs').argv;
 var concat = require('gulp-concat');
 var del = require('del');
-var eventStream = require('event-stream');
 var less = require('gulp-less');
 var path = require('path');
 var templateCache = require('gulp-angular-templatecache');
@@ -15,6 +14,7 @@ var hawtio = require('@hawtio/node-backend');
 var packageJson = require('./package.json');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
+var merge2 = require('merge2');
 
 var config = {
   dist: argv.out || './dist/',
@@ -27,29 +27,16 @@ gulp.task('clean', function () {
   }
 });
 
-gulp.task('tsc', ['clean'], function() {
+gulp.task('tsc', ['clean'], function () {
+  var templates = gulp.src(['src/app/**/*.html', 'src/app/**/*.md']).pipe(templateCache({ module: 'hawtio-core' }));
   var tsResult = tsProject.src().pipe(tsProject());
-  return eventStream.merge(
-    tsResult.js.pipe(ngAnnotate()),
-    tsResult.dts
-  )
-  .pipe(gulp.dest(config.dist));
+  var js = merge2(tsResult.js.pipe(ngAnnotate()), templates)
+    .pipe(concat('hawtio-core.js'))
+  return merge2(js, tsResult.dts)
+    .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('templates', ['tsc'], function() {
-  return eventStream.merge(
-    gulp.src([config.dist + 'hawtio-core.js']),
-    gulp
-      .src(['src/app/**/*.html', 'src/app/**/*.md'])
-      .pipe(templateCache({
-        module: 'hawtio-core'
-      }))
-  )
-  .pipe(concat('hawtio-core.js'))
-  .pipe(gulp.dest(config.dist));
-});
-
-gulp.task('copy-images', ['clean'], function() {
+gulp.task('copy-images', ['clean'], function () {
   return gulp.src('src/assets/img/**/*')
     .pipe(gulp.dest(config.dist + 'img'));
 });
@@ -66,17 +53,17 @@ gulp.task('less', ['clean'], function () {
     .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('clean-example', function() {
+gulp.task('clean-example', function () {
   return del(config.distExample);
 });
 
-gulp.task('hawtio-core-types', ['tsc'], function() {
+gulp.task('hawtio-core-types', ['tsc'], function () {
   gulp.src("./dist/hawtio-core.d.ts")
     .pipe(rename("index.d.ts"))
     .pipe(gulp.dest("./dist-example/types/hawtio-core"));
 });
 
-gulp.task('tsc-example', ['clean-example', 'hawtio-core-types'], function() {
+gulp.task('tsc-example', ['clean-example', 'hawtio-core-types'], function () {
   tsProjectExample.src()
     .pipe(tsProjectExample())
     .js
@@ -84,7 +71,7 @@ gulp.task('tsc-example', ['clean-example', 'hawtio-core-types'], function() {
     .pipe(gulp.dest(config.distExample));
 });    
 
-gulp.task('connect', ['build-example'], function() {
+gulp.task('connect', ['build-example'], function () {
   hawtio.setConfig({
     port: 2772,
     staticAssets: [{
@@ -96,27 +83,27 @@ gulp.task('connect', ['build-example'], function() {
       enabled: true
     }
   });
-  hawtio.use('/', function(req, res, next) {
-          var path = req.originalUrl;
-          if (path === '/') {
-            res.redirect('/hawtio/');
-          } else {
-            next();
-          }
-        });
-  hawtio.listen(function(server) {
+  hawtio.use('/', function (req, res, next) {
+    var path = req.originalUrl;
+    if (path === '/') {
+      res.redirect('/hawtio/');
+    } else {
+      next();
+    }
+  });
+  hawtio.listen(function (server) {
     var host = server.address().address;
     var port = server.address().port;
     console.log("started from gulp file at ", host, ":", port);
   });
 });
 
-gulp.task('reload', function() {
+gulp.task('reload', function () {
   gulp.src('.')
     .pipe(hawtio.reload());
 });
 
-gulp.task('watch', ['build-example'], function() {
+gulp.task('watch', ['build-example'], function () {
   gulp.watch(['src/**/*'], ['build-example']);
   gulp.watch(['index.html', config.distExample + '**/*'], ['reload']);
 });
@@ -127,12 +114,12 @@ gulp.task('test', ['build'], function (done) {
   }, done).start();
 });
 
-gulp.task('version', function() {
+gulp.task('version', function () {
   gulp.src(config.dist + 'hawtio-core.js')
     .pipe(replace('PACKAGE_VERSION_PLACEHOLDER', packageJson.version))
     .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('build', ['tsc', 'templates', 'copy-images', 'less']);
+gulp.task('build', ['tsc', 'copy-images', 'less']);
 gulp.task('build-example', ['build', 'tsc-example']);
 gulp.task('default', ['connect', 'watch']);

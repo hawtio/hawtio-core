f58a028375a999e4733b8ee9e54e285625adc392
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-03-03 14:10:40 +0900

    chore: change checkbox to switch in general prefs

--- src/app/preferences/general-preferences/general-preferences.service.ts
+++ src/app/preferences/general-preferences/general-preferences.service.ts
@@ -1,9 +1,13 @@
 namespace Core {
+
   export class GeneralPreferencesService {
+
     private static readonly DEFAULT_VERTICAL_NAV_STATE = "show";
+
     constructor(private $window: ng.IWindowService) {
       'ngInject';
     }
+
     getDefaultVerticalNavState(): string {
       if (_.isNull(this.$window.localStorage.getItem('defaultVerticalNavState'))) {
         return GeneralPreferencesService.DEFAULT_VERTICAL_NAV_STATE;
@@ -11,10 +15,10 @@ namespace Core {
         return this.$window.localStorage.getItem('defaultVerticalNavState');
       }
     }
-    setDefaultVerticalNavState(defaultVerticalNavState: string): void {
 
+    setDefaultVerticalNavState(defaultVerticalNavState: string): void {
       this.$window.localStorage.setItem('defaultVerticalNavState', defaultVerticalNavState);
     }
-
   }
+
 }

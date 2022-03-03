6f436acf98d5c2287d5d8629cc4e09c0257c3a36
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-03-03 14:10:40 +0900

    chore: change checkbox to switch in general prefs

--- src/app/preferences/general-preferences/general-preferences.controller.ts
+++ src/app/preferences/general-preferences/general-preferences.controller.ts
@@ -1,9 +1,20 @@
+/// <reference path="general-preferences.service.ts"/>
+
 namespace Core {
+
   export function GeneralPreferencesController($scope, generalPreferencesService: GeneralPreferencesService) {
     'ngInject';
-    $scope.defaultVerticalNavState = generalPreferencesService.getDefaultVerticalNavState();
-    $scope.onToggle = function () {
-      generalPreferencesService.setDefaultVerticalNavState($scope.defaultVerticalNavState);
-    }
+
+    const onSwitchChange = (_event, state) => {
+      generalPreferencesService.setDefaultVerticalNavState(state ? 'show' : 'hide');
+    };
+
+    (jQuery("#general-preferences-form-vertical-nav-switch") as any).bootstrapSwitch({
+      onText: 'Show',
+      offText: 'Hide',
+      state: generalPreferencesService.getDefaultVerticalNavState() === 'show',
+      onSwitchChange: onSwitchChange
+    });
   }
+
 }
